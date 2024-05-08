971d394993cd30f3a55ce4739e9ec4455862032a
Rohit Ganji <rohitganji@Rohits-MacBook-Pro.local>
2024-05-08 18:41:20 -0500

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDb from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 1313;
+
+dotenv.config()
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
+
+app.listen(PORT, () => {
+    connectToMongoDb();
+    console.log(`Server running on port ${PORT}`)
+});
