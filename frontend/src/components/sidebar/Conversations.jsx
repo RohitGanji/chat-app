6a687abff6090eb2e106fe78f5c87d773c8a3ab1
Rohit Ganji <rohitganji@Rohits-MacBook-Pro.local>
2024-05-08 22:21:04 -0500

    2nd part completed - ui design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,16 @@
+import Conversation from './Conversation';
+
+const Conversations = () => {
+  return (
+    <div className='py-2 flex flex-col overflorw-auto'>
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+    </div>
+  )
+}
+
+export default Conversations
