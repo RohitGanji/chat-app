ec4148932d8f8df3d3dfc7015719c30b5e9d796b
Rohit Ganji <rohitganji@Rohits-MacBook-Pro.local>
2024-05-11 10:38:24 -0400

    Socket updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:1313", {
+            const socket = io("https://chat-app-kx9z.onrender.com", {
                 query: {
                     userId: authUser._id,
                 }
